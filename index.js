e1fadadd3bc10b22d671367b416859a22307c8e8
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-17 16:14:40 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// Function to create thank you cards
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// Function to count down from a given number
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Example usage of writeCards
+const thankYouCards = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(thankYouCards);
+
+// Example usage of countDown
+countDown(10);
